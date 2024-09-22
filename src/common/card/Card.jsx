6e0311419e727b48bd7c6352a7a6da201f7ec92c
KANA FUJIHIRA <kanapedrinisfujihira@gmail.com>
2024-09-22 19:29:38 +0200

    add API for cat images and npm package for random cat name

--- src/common/card/Card.jsx
+++ src/common/card/Card.jsx
@@ -1,33 +1,51 @@
 import { useState } from "react";
+import { useLoaderData } from "react-router-dom";
 import StarEmpty from "../../assets/img/starempty.svg";
 import StarFavorited from "../../assets/img/starfavorited.svg";
+import { randomCatName } from "cat-names"; //npm gives one random name
 import styles from "../card/card.module.css";
 
 function Card() {
-  const [favorite, setFavorite] = useState(false);
+  // Initialize state as an empty object to track favorite status for each cat
+  const [favorites, setFavorites] = useState({});
 
-  const handleFavorite = () => {
-    setFavorite(!favorite);
+  // Toggle favorite status for a specific cat by its id
+  const handleFavorite = (catId) => {
+    setFavorites((prevFavorites) => ({
+      ...prevFavorites,
+      [catId]: !prevFavorites[catId], // Toggle the favorite status
+    }));
   };
+
+  const allCats = useLoaderData();
 
   return (
     <>
-      <div className={styles.cardContainer}>
-        <o>here will be cat image</o>
-        {favorite === false ? (
-          <img
-            onClick={handleFavorite}
-            src={StarEmpty}
-            className={styles.buttonStar}
-          />
-        ) : (
-          <img
-            onClick={handleFavorite}
-            src={StarFavorited}
-            className={styles.buttonStar}
-          />
-        )}
-      </div>
+      {allCats.map((cat) => {
+        // Generate a random name for each cat
+        const randomCat = randomCatName();
+
+        return (
+          <div className={styles.cardContainer} key={cat.id}>
+            <img src={cat.url} className={styles.catImageSize} alt={cat.id} />
+            {/* Display the random name for each cat */}
+            <p className={styles.catName}>{randomCat}</p>
+            {favorites[cat.id] ? (
+              <img
+                onClick={() => handleFavorite(cat.id)}
+                src={StarFavorited}
+                className={styles.buttonStar}
+              />
+            ) : (
+              <img
+                onClick={() => handleFavorite(cat.id)}
+                src={StarEmpty}
+                className={styles.buttonStar}
+              />
+            )}
+          </div>
+        );
+      })}
     </>
   );
 }
